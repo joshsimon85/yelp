dd6db74a11f52dfdbf78e13dd1837cdd34f9469f
Josh <joshsimon85@yahoo.com>
2019-07-15 22:36:24 -0600

    run db mirgrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,24 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_10_182141) do
+ActiveRecord::Schema.define(version: 2019_07_12_152823) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "businesses", force: :cascade do |t|
+    t.string "name"
+    t.string "address_1"
+    t.string "address_2"
+    t.string "city"
+    t.string "state"
+    t.string "phone"
+    t.text "tags"
+    t.integer "price"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.text "description"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "first_name"
