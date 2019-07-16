917b41398aeec1db1b60cbcfb55416314dcc115b
Josh <joshsimon85@yahoo.com>
2019-07-15 22:09:41 -0600

    Add business show controller tests

--- app/controllers/businesses_controller.rb
+++ app/controllers/businesses_controller.rb
@@ -2,7 +2,7 @@ class BusinessesController < ApplicationController
   def index
     @businesses = Business.get_records_by_offset(params[:page], 5)
     @count = Business.count
-    @current_page = params[:page] || 1
+    @current_page = params[:page] || '1'
   end
 
   def show
